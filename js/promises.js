d63d3276f2b560a51ea5014eeae3efb94ff0b4ad
Matt. Toczek <toczek.matt@gmail.com>
2019-09-18 16:15:38 +0100

    adding .then and .catch to event listener to repond to promise and adding return functions to getProfiles()

--- js/promises.js
+++ js/promises.js
@@ -22,9 +22,10 @@ function getJSON(url) {
 }
 
 function getProfiles(json) {
-  json.people.map( person => {
-    getJSON(wikiUrl + person.name, generateHTML);      
+  const profiles = json.people.map( person => {
+    return getJSON(wikiUrl + person.name);  
   }); 
+  return profiles;    
 }
 
 function generateHTML(data) {
@@ -39,6 +40,10 @@ function generateHTML(data) {
 }
 
 btn.addEventListener('click', (event) => {
-  getJSON(astrosUrl, getProfiles);
+  getJSON(astrosUrl)
+    .then(getProfiles)
+    .then( data => console.log(data) )
+    .catch( err => console.log(err) )
+    
   event.target.remove();
 });
